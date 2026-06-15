b086605d005bb639bdcd207293c1a881d805534e
Claudio Esperança <claudio.esperanca@gmail.com>
2026-06-15 18:31:29 -0300

    teste com loadImage
    Teste com carga de imagem a partir de uma url relativa

--- image_convolution/sketch.py
+++ image_convolution/sketch.py
@@ -1,5 +1,7 @@
 """
 Demonstrates the application of 3x3 convolution kernels on assorted images.
+Disclaimer: All images were downloaded from 
+https://www.hlevkin.com/hlevkin/TestImages/
 """
 from gui import GuiBlock
 import math
@@ -15,12 +17,20 @@ kernels = {
     "sobel_v": [-1, -2, -1, 0, 0, 0, 1, 2, 1]
 }
 
-image_names = [
-    "lenna.bmp",
-    "cameraman.bmp",
-    "baboon.bmp",
-    "pepper.bmp"
-]
+image_dict = {
+    "airfield2.bmp": None,
+    "baboon.bmp": None,
+    "boats.bmp" : None,
+    "cameraman.bmp": None,
+    "finger.bmp": None,
+    "flower.bmp" : None,
+    "girl.bmp" : None,
+    "houses.bmp" : None, 
+    "pens.bmp" : None,
+    "lenna.bmp" : None,
+    "lighthouse.bmp" : None,
+    "pepper.bmp": None,
+}
 
 # How the convolution treats color. Maps the GUI label to the shader's u_mode.
 color_modes = {
@@ -32,10 +42,10 @@ color_modes = {
 def preload():
     global my_shader
     my_shader = loadShader("vert.glsl", "frag.glsl")
-    global img
-    img = {}
-    for name in image_names:
-        img[name] = load_image(name)
+    # global img
+    # img = {}
+    # for name in image_names:
+    #     img[name] = load_image(name)
 
 def setup():
     createCanvas(windowWidth, windowHeight, WEBGL)
@@ -44,7 +54,7 @@ def setup():
     global gui
     gui = GuiBlock()
     gui.addSelect("kernel", kernels.keys(), "identity")
-    gui.addSelect("image", img.keys(), "cameraman.bmp")
+    gui.addSelect("image", image_dict.keys(), "cameraman.bmp")
     gui.addSelect("mode", color_modes.keys(), "rgb")
     gui.change(update_kernel)
     
@@ -75,7 +85,28 @@ def setup():
 
     apply_convolution()
     
+def handle_image(img):
+    image_dict [gui.image] = img
+    print ("loaded", gui.image)
+    apply_convolution()
+
+def handle_fail(event):
+    print ("failed: ")
+    print (event)
+  
+load_callback = create_proxy(handle_image)
+fail_callback = create_proxy(handle_fail)
+
 def apply_convolution():
+    src = image_dict [gui.image]
+    # Load image on demand
+    if src is None:
+        #url = 'https://esperanc.github.io/Py5Sketches/image_convolution/'+gui.image
+        url = './'+gui.image
+        loadImage(url, load_callback, fail_callback)
+        return
+        
+    # Decode the convolution kernel
     try:
         kernel=[]
         for kd in kernel_display:
@@ -85,14 +116,17 @@ def apply_convolution():
     except Exception as e:
         print (e)
         return
+    
+    # Adapt the frame buffer to the image source
     global fb
-    src = img[gui.image]
     if fb == None or fb.width != src.width or fb.height != fb.height:
         if fb != None: fb.remove()
         fb = create_framebuffer(js_object(
             {"width": src.width,
              "height": src.height,
              "density": 1}))
+    
+    # Apply the kernel
     fb.begin()
     clear()
     shader(my_shader)
@@ -114,6 +148,7 @@ def update_kernel():
 
 def draw():
     background(240)
+    if not fb: return
     sfactor = min(width/fb.width,height/fb.height)*0.9
     scale(sfactor)
     image(fb,-fb.width/2,-fb.height/2)
